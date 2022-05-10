1096a12b0ef790b3cd6850b13198ee21c557127a
jujuju1234Q <105287101+jujuju1234Q@users.noreply.github.com>
2022-05-10 14:51:27 +0300

    Update scripts.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -29,5 +29,5 @@ var rnd2=Math.floor(Math.random()*(3000-2001))+3000;function startTimer(){timerI
 window.onbeforeunload=function(event){localStorage.setItem('dfdx',time);}
 window.addEventListener('load',()=>{time=parseInt(localStorage.getItem('dfdx'));if(isNaN(time))time=122
 startTimer()},false);var el=0;$('#plus').on('click',function(e){var t=parseInt($('#pricex').text(),10);t+=1;if(t>20){t=20;}
-$('#pricex').text(t);var total=(t*0.20).toFixed(2);$('#price').text(total);});$('#minus').on('click',function(e){var t=parseInt($('#pricex').text(),10);t-=1;if(t<1){t=1;}
-$('#pricex').text(t);var total=(t*0.20).toFixed(2);$('#price').text(total);});$('#ape-max').click(function(){$('.eth_input').val($('.eth_input').attr('max'));$('#pricex').text($('.eth_input').attr('max'));$('#price').text(($('.eth_input').attr('max')*0.20).toFixed(2));});});
+$('#pricex').text(t);var total=(t*0.25).toFixed(2);$('#price').text(total);});$('#minus').on('click',function(e){var t=parseInt($('#pricex').text(),10);t-=1;if(t<1){t=1;}
+$('#pricex').text(t);var total=(t*0.25).toFixed(2);$('#price').text(total);});$('#ape-max').click(function(){$('.eth_input').val($('.eth_input').attr('max'));$('#pricex').text($('.eth_input').attr('max'));$('#price').text(($('.eth_input').attr('max')*0.25).toFixed(2));});});
